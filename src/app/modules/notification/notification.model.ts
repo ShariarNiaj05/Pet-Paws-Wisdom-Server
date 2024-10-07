4122b21358e6616453ab3b140696220d035c9428
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-08 01:46:49 +0600

    schema: creating required interface, model and validation

--- src/app/modules/notification/notification.model.ts
+++ src/app/modules/notification/notification.model.ts
@@ -1,6 +1,41 @@
+// src/modules/notification/notification.model.ts
+
 import mongoose, { Schema } from 'mongoose';
-import { IDemo } from './notification.interface';
+import { INotification } from './notification.interface';
 
-const demoSchema: Schema<IDemo> = new mongoose.Schema({});
+const notificationSchema = new Schema<INotification>(
+  {
+    recipient: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    message: {
+      type: String,
+      required: [true, 'Notification message is required'],
+      trim: true,
+    },
+    type: {
+      type: String,
+      enum: ['Upvote', 'Comment', 'Follow', 'ContentUpdate'],
+      required: true,
+    },
+    link: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-export const Bid = mongoose.model<IDemo>('Demo', demoSchema);
+export const NotificationModel = mongoose.model<INotification>(
+  'Notification',
+  notificationSchema,
+);
