474f3f0822380f9c0be356eef7bf68bddf7043e2
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-08 01:32:45 +0600

    schema: creating required interface, model and validation

--- src/app/modules/comment/comment.model.ts
+++ src/app/modules/comment/comment.model.ts
@@ -1,6 +1,28 @@
 import mongoose, { Schema } from 'mongoose';
-import { IDemo } from './comment.interface';
+import { IComment } from './comment.interface';
 
-const demoSchema: Schema<IDemo> = new mongoose.Schema({});
+const commentSchema = new Schema<IComment>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+      required: true,
+    },
+    content: {
+      type: String,
+      required: [true, 'Comment content is required'],
+      trim: true,
+      maxlength: [1000, 'Comment content cannot exceed 1000 characters'],
+    },
+  },
+  {
+    timestamps: true, // Automatically adds `createdAt` and `updatedAt` fields
+  },
+);
 
-export const Bid = mongoose.model<IDemo>('Demo', demoSchema);
+export const CommentModel = mongoose.model<IComment>('Comment', commentSchema);
