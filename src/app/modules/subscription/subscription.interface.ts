5f622c310daf4f529ce530d4626704a56a8934f5
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-08 01:53:38 +0600

    schema: creating required interface, model and validation

--- src/app/modules/subscription/subscription.interface.ts
+++ src/app/modules/subscription/subscription.interface.ts
@@ -1,1 +1,12 @@
-export interface IDemo {}
+import { Types } from 'mongoose';
+
+export interface ISubscription {
+  userId: Types.ObjectId;
+  plan: 'basic' | 'premium';
+  startDate: Date;
+  endDate: Date;
+  status: 'active' | 'expired' | 'cancelled';
+  paymentId: Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
