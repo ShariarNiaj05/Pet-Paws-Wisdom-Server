a381e4564e1791f702832c9ded9ff4b3a925e6a9
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-08 01:37:43 +0600

    schema: creating required interface, model and validation

--- src/app/modules/content/content.interface.ts
+++ src/app/modules/content/content.interface.ts
@@ -1,1 +1,13 @@
-export interface IDemo {}
+import { Types } from 'mongoose';
+
+export interface IContent {
+  author: Types.ObjectId;
+  title: string;
+  body: string;
+  category: Types.ObjectId;
+  tags?: string[];
+  isPremium: boolean;
+  type: 'Tip' | 'Story';
+  createdAt?: Date;
+  updatedAt?: Date;
+}
