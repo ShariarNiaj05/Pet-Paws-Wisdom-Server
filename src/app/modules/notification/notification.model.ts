0a2a0617eefc005561e27600eb1968bcaa3e9641
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-08 00:54:06 +0600

    init: creating required services

--- src/app/modules/notification/notification.model.ts
+++ src/app/modules/notification/notification.model.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
-import { IDemo } from './demo.interface';
+import { IDemo } from './notification.interface';
 
 const demoSchema: Schema<IDemo> = new mongoose.Schema({});
 
