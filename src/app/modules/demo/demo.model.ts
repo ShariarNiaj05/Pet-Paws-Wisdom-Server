99cc2aeefcaf4fff0bab6ac114d7ff11fffd2fb0
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-07 23:56:05 +0600

    cleanup: cleaning the the initial project

--- src/app/modules/demo/demo.model.ts
+++ src/app/modules/demo/demo.model.ts
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
+import { IDemo } from './demo.interface';
 
-const demoSchema: Schema<> = new mongoose.Schema({});
+const demoSchema: Schema<IDemo> = new mongoose.Schema({});
 
-export const Bid = mongoose.model<>('Bid', demoSchema);
+export const Bid = mongoose.model<IDemo>('Demo', demoSchema);
