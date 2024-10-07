7732f9e8e76f24b82e9c4f9c00ea1263812e99a1
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-07 23:55:12 +0600

    cleanup: cleaning the the initial project

--- src/app/modules/demo/demo.model.ts
+++ src/app/modules/demo/demo.model.ts
@@ -1,23 +1,5 @@
-/* eslint-disable no-useless-escape */
-// models/Bid.ts
 import mongoose, { Schema } from 'mongoose';
-import { IBid } from './demo.interface';
 
-const bidSchema: Schema<IBid> = new mongoose.Schema({
-  rentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Rent', required: true },
+const demoSchema: Schema<> = new mongoose.Schema({});
 
-  driverId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Bid',
-    required: true,
-  },
-  bidAmount: { type: Number, required: true },
-  bidStatus: {
-    type: String,
-    enum: ['accepted', 'pending', 'rejected'],
-    default: 'pending',
-  },
-  driverLocation: { type: String, required: true },
-});
-
-export const Bid = mongoose.model<IBid>('Bid', bidSchema);
+export const Bid = mongoose.model<>('Bid', demoSchema);
