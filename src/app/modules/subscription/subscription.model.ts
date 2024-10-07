f3ad9964e8fabe5c8ddb03cdef6762c5cc685c0f
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-08 01:54:21 +0600

    schema: creating required interface, model and validation

--- src/app/modules/subscription/subscription.model.ts
+++ src/app/modules/subscription/subscription.model.ts
@@ -1,6 +1,44 @@
 import mongoose, { Schema } from 'mongoose';
-import { IDemo } from './subscription.interface';
+import { ISubscription } from './subscription.interface';
 
-const demoSchema: Schema<IDemo> = new mongoose.Schema({});
+const subscriptionSchema = new Schema<ISubscription>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    plan: {
+      type: String,
+      enum: ['basic', 'premium'],
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['active', 'expired', 'cancelled'],
+      required: true,
+      default: 'active',
+    },
+    paymentId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Payment',
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Automatically creates `createdAt` and `updatedAt` fields
+  },
+);
 
-export const Bid = mongoose.model<IDemo>('Demo', demoSchema);
+export const SubscriptionModel = mongoose.model<ISubscription>(
+  'Subscription',
+  subscriptionSchema,
+);
