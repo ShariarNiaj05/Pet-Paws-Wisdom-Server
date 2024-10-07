8916297cac732662be5b0014a133e509f214be19
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-08 01:51:05 +0600

    schema: creating required interface, model and validation

--- src/app/modules/payment/payment.model.ts
+++ src/app/modules/payment/payment.model.ts
@@ -1,6 +1,40 @@
 import mongoose, { Schema } from 'mongoose';
-import { IDemo } from './payment.interface';
+import { IPayment } from './payment.interface';
 
-const demoSchema: Schema<IDemo> = new mongoose.Schema({});
+const paymentSchema = new Schema<IPayment>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    currency: {
+      type: String,
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      enum: ['Stripe', 'Aamarpay'],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'completed', 'failed'],
+      default: 'pending',
+      required: true,
+    },
+    transactionId: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true, // Adds createdAt and updatedAt timestamps automatically
+  },
+);
 
-export const Bid = mongoose.model<IDemo>('Demo', demoSchema);
+export const PaymentModel = mongoose.model<IPayment>('Payment', paymentSchema);
