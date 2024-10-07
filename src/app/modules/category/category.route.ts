717fc9e27cc4a279955af15ca4ea8db2b9f86a81
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-08 00:52:19 +0600

    init: creating required services

--- src/app/modules/category/category.route.ts
+++ src/app/modules/category/category.route.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
-import { BidController } from './bid.controller';
+import { BidController } from './category.controller';
 import {
   createBidValidationSchema,
   updateBidValidationSchema,
-} from './bid.validation';
+} from './category.validation';
 import auth from '../../middlewares/auth';
 import { USER_ROLE } from '../User/user.utils';
 
--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -1,7 +1,7 @@
 import QueryBuilder from '../../builder/QueryBuilder';
 import { Rent } from '../Rent/rent.model';
 import { IBid } from './category.interface';
-import { Bid } from './bid.model';
+import { Bid } from './category.model';
 
 const createBid = async (bid: IBid) => {
   return await Bid.create(bid);
