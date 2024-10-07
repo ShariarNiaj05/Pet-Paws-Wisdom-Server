7fae8f3fb9d60eecfdb53fdccf525da1700979cc
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-08 00:58:56 +0600

    init: creating required services

--- src/app/modules/content/content.model.ts
+++ src/app/modules/content/content.model.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
-import { IDemo } from './demo.interface';
+import { IDemo } from './content.interface';
 
 const demoSchema: Schema<IDemo> = new mongoose.Schema({});
 
