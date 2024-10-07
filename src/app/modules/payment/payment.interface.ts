c92dba9b9cb9481d4e673c87f9c069bd5cb6cb67
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-08 01:50:37 +0600

    schema: creating required interface, model and validation

--- src/app/modules/payment/payment.interface.ts
+++ src/app/modules/payment/payment.interface.ts
@@ -1,1 +1,12 @@
-export interface IDemo {}
+import { Types } from 'mongoose';
+
+export interface IPayment {
+  userId: Types.ObjectId;
+  amount: number;
+  currency: string;
+  paymentMethod: 'Stripe' | 'Aamarpay';
+  status: 'pending' | 'completed' | 'failed';
+  transactionId?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
