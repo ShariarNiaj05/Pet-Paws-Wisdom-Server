4942cea0520ee8d0da4370b1c4f6b33c89d99119
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-07 23:55:35 +0600

    cleanup: cleaning the the initial project

--- src/app/modules/demo/demo.interface.ts
+++ src/app/modules/demo/demo.interface.ts
@@ -1,9 +1,3 @@
 import { Types } from 'mongoose';
 
-export interface IBid {
-  rentId: Types.ObjectId;
-  driverId: Types.ObjectId;
-  bidAmount: number;
-  bidStatus: 'accepted' | 'pending' | 'rejected';
-  driverLocation: string;
-}
+export interface IDemo {}
