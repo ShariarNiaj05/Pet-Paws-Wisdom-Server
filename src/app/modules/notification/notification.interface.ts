7a5a0857e3ed6e470edd52b873cf93242b731cb8
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-08 01:45:58 +0600

    schema: creating required interface, model and validation

--- src/app/modules/notification/notification.interface.ts
+++ src/app/modules/notification/notification.interface.ts
@@ -1,1 +1,11 @@
-export interface IDemo {}
+import { Types } from 'mongoose';
+
+export interface INotification {
+  recipient: Types.ObjectId;
+  message: string;
+  type: 'Upvote' | 'Comment' | 'Follow' | 'ContentUpdate';
+  link?: string;
+  isRead: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
