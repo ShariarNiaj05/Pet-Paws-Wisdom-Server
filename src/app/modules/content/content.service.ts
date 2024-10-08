907159884db9f9d22ae7b2f79339339efb07b294
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 01:29:56 +0600

    API: adding required content api

--- src/app/modules/content/content.service.ts
+++ src/app/modules/content/content.service.ts
@@ -1,1 +1,9 @@
+import { IContent } from './content.interface';
+import { ContentModel } from './content.model';
+
+const createContentIntoDB = async (data: IContent, userId: string) => {
+  const newContent = await ContentModel.create({ ...data, author: userId });
+  return newContent;
+};
+
 export const ContentService = {};
