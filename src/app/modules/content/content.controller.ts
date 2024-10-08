38a9043dd72c60bd789379fb70380cbfe4c21a65
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 01:35:56 +0600

    API: adding required content api

--- src/app/modules/content/content.controller.ts
+++ src/app/modules/content/content.controller.ts
@@ -1,1 +1,20 @@
-export const ContentController = {};
+import { Request, Response } from 'express';
+import catchAsync from '../../utils/catchAsync';
+import { ContentService } from './content.service';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
+
+const createContent = catchAsync(async (req: Request, res: Response) => {
+  const result = await ContentService.createContentIntoDB(
+    req.body,
+    req.user?._id,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: 'Content created successfully',
+    data: result,
+  });
+});
+
+export const ContentController = { createContent };
