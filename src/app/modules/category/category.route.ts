eadd7620c1d27d075b5886fea1702c9318aad7b8
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 00:09:19 +0600

    API: adding required category api

--- src/app/modules/category/category.route.ts
+++ src/app/modules/category/category.route.ts
@@ -2,10 +2,17 @@ import express from 'express';
 import { CategoryController } from './category.controller';
 import auth from '../../middlewares/auth';
 import { USER_ROLE } from '../User/user.utils';
+import validateRequest from '../../middlewares/validateRequest';
+import { CategoryValidations } from './category.validation';
 
 const router = express.Router();
 router.get('/', CategoryController.getAllCategories);
 router.get('/:id', CategoryController.getCategoryById);
-router.post('/', auth(USER_ROLE.admin), CategoryController.createCategory);
+router.post(
+  '/',
+  auth(USER_ROLE.admin),
+  validateRequest(CategoryValidations.createCategoryValidationSchema),
+  CategoryController.createCategory,
+);
 
 export const CategoryRoutes = router;
