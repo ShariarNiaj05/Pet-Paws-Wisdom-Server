01db16c9c8d672c7514e3d1c638a935576a7179b
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 00:01:56 +0600

    API: adding category services

--- src/app/modules/category/category.route.ts
+++ src/app/modules/category/category.route.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import { CategoryController } from './category.controller';
 
 const router = express.Router();
+router.get('/', CategoryController.getAllCategories);
 
-export const DemoRoutes = router;
+export const CategoryRoutes = router;
