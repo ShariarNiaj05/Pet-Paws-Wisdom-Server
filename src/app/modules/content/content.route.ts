b9f1994f30115e000c537ef6cafa8db89ea66ab4
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 01:38:04 +0600

    API: adding required content api

--- src/app/modules/content/content.route.ts
+++ src/app/modules/content/content.route.ts
@@ -1,5 +1,16 @@
 import express from 'express';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { ContentValidations } from './content.validation';
+import { ContentController } from './content.controller';
+import { USER_ROLE } from '../User/user.utils';
 
 const router = express.Router();
+router.post(
+  '/',
+  auth(USER_ROLE.user),
+  validateRequest(ContentValidations.createContentValidationSchema),
+  ContentController.createContent,
+);
 
 export const ContentRoutes = router;
