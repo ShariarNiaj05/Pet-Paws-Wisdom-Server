83c58709ce15d78dabea593b5bbfa7ac27890706
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 00:01:24 +0600

    API: adding category services

--- src/app/modules/category/category.controller.ts
+++ src/app/modules/category/category.controller.ts
@@ -1,1 +1,17 @@
-export const CategoryController = {};
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { CategoryService } from './category.service';
+
+const getAllCategories = catchAsync(async (req, res) => {
+  const result = await CategoryService.getAllCategoriesFromDB(req.query);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Categories retrieved successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+export const CategoryController = { getAllCategories };
--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -1,1 +1,20 @@
-export const CategoryService = {};
+import QueryBuilder from '../../builder/QueryBuilder';
+import { CategoryModel } from './category.model';
+
+const getAllCategoriesFromDB = async (query: Record<string, unknown>) => {
+  const categoryQuery = new QueryBuilder(CategoryModel.find(), query)
+    .search(['name'])
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await categoryQuery.modelQuery;
+  const metaData = await categoryQuery.countTotal();
+  return {
+    meta: metaData,
+    data: result,
+  };
+};
+
+export const CategoryService = { getAllCategoriesFromDB };
