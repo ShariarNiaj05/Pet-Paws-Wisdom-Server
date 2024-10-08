34b253e17f3028283efbbed58620874fa2e0586d
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-08 23:28:46 +0600

    clenup: cleaning index file

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,5 @@
 import { Router } from 'express';
 import { AuthRoutes } from '../modules/Auth/auth.route';
-import { BidRoutes } from '../modules/Bid/bid.route';
-import { CarRoutes } from '../modules/Car/car.route';
-import { RentRoutes } from '../modules/Rent/rent.route';
 import { UserRoutes } from '../modules/User/user.route';
 
 type TModuleRoutes = {
