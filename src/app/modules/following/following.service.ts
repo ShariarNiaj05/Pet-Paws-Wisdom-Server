3b713e37a3d7695d6d0cf9cb46eed5c5c828969a
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-10 00:15:07 +0600

    API: all the following  APIs

--- src/app/modules/following/following.service.ts
+++ src/app/modules/following/following.service.ts
@@ -1,1 +1,33 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { User } from '../User/user.model';
+
+const followUser = async (userId: string, targetUserId: string) => {
+  if (!targetUserId) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Invalid user ID');
+  }
+
+  const targetUser = await User.findById(targetUserId);
+  if (!targetUser) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found');
+  }
+
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found');
+  }
+
+  if (user.following.includes(targetUserId)) {
+    throw new Error('Already following this user');
+  }
+
+  user.following.push(targetUserId);
+  targetUser.followers.push(userId);
+
+  await user.save();
+  await targetUser.save();
+
+  return { following: user.following, followers: targetUser.followers };
+};
+
 export const FollowingService = {};
