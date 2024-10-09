4c773c0667eb1b036fb35ddd939e2a24be0ddea6
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-10 00:17:42 +0600

    API: all the following  APIs

--- src/app/modules/following/following.controller.ts
+++ src/app/modules/following/following.controller.ts
@@ -1,3 +1,7 @@
+import httpStatus from 'http-status';
+import sendResponse from '../../utils/sendResponse';
+import { FollowingService } from './following.service';
+
 const followUser = catchAsync(async (req: Request, res: Response) => {
   const userId = req.user._id;
   const targetUserId = req.params.userId;
