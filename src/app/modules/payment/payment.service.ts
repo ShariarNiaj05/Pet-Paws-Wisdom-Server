3ba6b336f64e28e40eadbef7aa9566265c7be275
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 23:52:51 +0600

    API: all the payment APIs

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -1,1 +1,8 @@
-export const PaymentService = {};
+import { PaymentModel } from './payment.model';
+
+const getPaymentsByUser = async (userId: string) => {
+  const payments = await PaymentModel.find({ userId });
+  return payments;
+};
+
+export const PaymentService = { getPaymentsByUser };
