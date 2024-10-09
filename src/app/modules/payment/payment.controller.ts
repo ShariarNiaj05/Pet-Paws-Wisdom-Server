cd9d2fcb4405bd8647cdd5ad836f2284cc9fffbd
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 23:53:13 +0600

    API: all the payment APIs

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -1,1 +1,14 @@
+const getPaymentsByUser = catchAsync(async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  const result = await PaymentService.getPaymentsByUser(userId);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Payments retrieved successfully',
+    data: result,
+  });
+});
+
 export const PaymentController = {};
