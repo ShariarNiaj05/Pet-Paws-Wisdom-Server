cea930b4e2e3f0fb3402a08b2561879e7428dde0
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-10 00:47:42 +0600

    API: all the notification  APIs

--- src/app/modules/notification/notification.service.ts
+++ src/app/modules/notification/notification.service.ts
@@ -1,1 +1,9 @@
+import { INotification } from './notification.interface';
+import { NotificationModel } from './notification.model';
+
+const createNotification = async (data: INotification) => {
+  const notification = await NotificationModel.create(data);
+  return notification;
+};
+
 export const NotificationService = {};
