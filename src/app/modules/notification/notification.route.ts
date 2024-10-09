58b37483caf1e8dbb45b0b88776bc0e2ebc81989
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-10 00:50:29 +0600

    API: all the notification  APIs

--- src/app/modules/notification/notification.route.ts
+++ src/app/modules/notification/notification.route.ts
@@ -1,3 +1,13 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { NotificationValidations } from './notification.validation';
+
 const router = express.Router();
 
+router.post(
+  '/',
+  auth(),
+  validateRequest(NotificationValidations.createNotificationValidationSchema),
+  NotificationController.createNotification,
+);
 export const NotificationRoutes = router;
