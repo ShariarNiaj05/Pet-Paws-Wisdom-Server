18385bb8b3e49f1686363a6691bbfeab52113403
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 23:35:42 +0600

    API: all the subscription APIs

--- src/app/modules/subscription/subscription.service.ts
+++ src/app/modules/subscription/subscription.service.ts
@@ -1,1 +1,19 @@
+import { ISubscription } from './subscription.interface';
+import { SubscriptionModel } from './subscription.model';
+
+const createSubscriptionIntoDB = async (
+  userId: string,
+  plan: string,
+  paymentId: string,
+  endDate: Date,
+): Promise<ISubscription> => {
+  const newSubscription = await SubscriptionModel.create({
+    user: userId,
+    plan,
+    paymentId,
+    endDate,
+  });
+  return newSubscription;
+};
+
 export const SubscriptionService = {};
