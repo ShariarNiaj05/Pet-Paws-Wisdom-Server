518c0863d75b0352c777d91cf519b91a1ae27e78
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 22:28:51 +0600

    API: all the service for comment

--- src/app/modules/comment/comment.route.ts
+++ src/app/modules/comment/comment.route.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import { CommentController } from './comment.controller';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../User/user.utils';
 
 const router = express.Router();
 
 router.get('/:contentId', CommentController.getAllComments);
+
+router.post('/', auth(USER_ROLE.user), CommentController.createComment);
 
 export const CommentRoutes = router;
