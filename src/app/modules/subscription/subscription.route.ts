9540ad0f57a3c4f926ed3ff94ffb8deb9fe424f4
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 23:31:55 +0600

    API: all the subscription APIs

--- src/app/modules/subscription/subscription.route.ts
+++ src/app/modules/subscription/subscription.route.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../User/user.utils';
 
 const router = express.Router();
-
+router.post(
+  '/create',
+  auth(USER_ROLE.user),
+  validateRequest(SubscriptionValidations.createSubscriptionSchema),
+  SubscriptionController.createSubscription,
+);
 export const SubscriptionRoutes = router;
