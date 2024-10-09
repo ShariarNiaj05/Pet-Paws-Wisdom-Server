90dfd0f3b190dd0ad66259a129677b0ea52fe2b7
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 22:27:02 +0600

    API: all the service for comment

--- src/app/modules/comment/comment.controller.ts
+++ src/app/modules/comment/comment.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import catchAsync from '../../utils/catchAsync';
 import { CommentModel } from './comment.model';
+import sendResponse from '../../utils/sendResponse';
+import { CommentService } from './comment.service';
 
 const getAllComments = catchAsync(async (req: Request, res: Response) => {
   const { contentId } = req.params;
