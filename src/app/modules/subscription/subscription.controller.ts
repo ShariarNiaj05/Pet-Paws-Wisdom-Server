5644385b41c12cb14c6956e6f15841b09f4f73b7
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 23:34:23 +0600

    API: all the subscription APIs

--- src/app/modules/subscription/subscription.controller.ts
+++ src/app/modules/subscription/subscription.controller.ts
@@ -1,1 +1,24 @@
+import { Request, Response } from 'express';
+import catchAsync from '../../utils/catchAsync';
+import { SubscriptionService } from './subscription.service';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
+
+const createSubscription = catchAsync(async (req: Request, res: Response) => {
+  const { plan, paymentId, endDate } = req.body;
+  const result = await SubscriptionService.createSubscriptionIntoDB(
+    req.user._id,
+    plan,
+    paymentId,
+    endDate,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: 'Subscription created successfully',
+    data: result,
+  });
+});
+
 export const SubscriptionController = {};
