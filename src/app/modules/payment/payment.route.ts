62d21fc8a47b3127a10d4aeb8b2f789de521ff42
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-09 23:54:23 +0600

    API: all the payment APIs

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -1,5 +1,14 @@
 import express from 'express';
+import { USER_ROLE } from '../User/user.utils';
+import { PaymentController } from './payment.controller';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
+
+router.get(
+  '/:userId',
+  auth(USER_ROLE.user),
+  PaymentController.getPaymentsByUser,
+);
 
 export const PaymentRoutes = router;
