bdf28a9cd49d6082c37379d52bfe1ab156dc5279
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-10 00:49:02 +0600

    API: all the notification  APIs

--- src/app/modules/notification/notification.route.ts
+++ src/app/modules/notification/notification.route.ts
@@ -1,21 +1,3 @@
-import express, { Request, Response } from 'express';
-import { NotificationService } from './notification.service';
-import catchAsync from '../../utils/catchAsync';
-import sendResponse from '../../utils/sendResponse';
-import httpStatus from 'http-status';
-
 const router = express.Router();
 
-const createNotification = catchAsync(async (req: Request, res: Response) => {
-  const notification = await NotificationService.createNotificationIntoDB(
-    req.body,
-  );
-
-  sendResponse(res, {
-    statusCode: httpStatus.CREATED,
-    success: true,
-    message: 'Notification created successfully',
-    data: notification,
-  });
-});
 export const NotificationRoutes = router;
