b4fa44bdb49f5ab0e707629ebfb6bc3bc27d2203
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-11 19:54:51 +0600

    production url added

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,15 @@ const app: Application = express();
 //parsers
 app.use(express.json());
 
-app.use(cors({ origin: ['http://localhost:3000'] }));
-app.use(cookieParser())
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://pet-paws-wisdom-client.vercel.app',
+    ],
+  }),
+);
+app.use(cookieParser());
 // application routes
 app.use('/api/v1', router);
 
